d032dda32686b691458a3f93bf5e2ea20af4a95d
n2meetu <zwandyzf@gmail.com>
2018-09-25 11:21:08 +0800

    继承

--- LeaningProcessProject/继承.js
+++ LeaningProcessProject/继承.js
@@ -30,45 +30,35 @@ v1.showName();
 v1.showPass();
 v1.showLevel();
 
-// 再写一遍ES5版本的
-// 1.改造函数
-// var VipUser = function (name, pass, level) {
-//     User.call(this, name, pass);
-//     this.level = level;
-// }
-// // 2.原型链调整 prototype 和 constructor
-// VipUser.prototype = new User();
-// VipUser.prototype.constructor = VipUser;
-
-// // 3.原型对象
-// VipUser.prototype.showLevel = function () {
-//     console.log(this.level);
-// }
-
-// var v2 = new VipUser('greta','123123','8');
-// v2.showLevel();
-// v2.showName();
-// v2.showPass();
 
 // —————————— ES6 新增Class关键字 ————————————
 class User {
-    var 
+    constructor(name, pass) {
+        this.name = name;
+        this.pass = pass;
+    }
+
+    showName() {
+        alert(this.name);
+    }
+    showPass() {
+        alert(this.pass);
+    }
 }
 
-class VipUser extends User() {
-    constructor(name, pass, level) { // 子类也是类，constructor少不了
-        super(name, pass)  // 超类  作用等同于原来的 call；表示继承自User
+class VipUser extends User {
+    constructor(name, pass, level) {
+        super(name, pass);
         this.level = level;
     }
 
-    // 新方法
-    show() {
-        console.log(this.level);
+    showLevel() {
+        alert(this.level);
     }
 }
 
+var v1 = new VipUser('blue', '123456', 3);
 
-var v2 = new VipUser('greta', '123123', '8');
-v2.showLevel();
-v2.showName();
-v2.showPass();
+v1.showName();
+v1.showPass();
+v1.showLevel();
